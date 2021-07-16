acdc35102ca2ca73682cd4d513010244e2e1128d
gomberg5264 <61816796+gomberg5264@users.noreply.github.com>
2021-07-16 13:27:59 -0400

    Update backend.php

--- backend.php
+++ backend.php
@@ -26,3 +26,62 @@ class cms{
 
     return $this->buildDB();
   } 
+   public function display_admin() {
+    return <<<ADMIN_FORM
+
+    <form action="{$_SERVER['PHP_SELF']}" method="post">
+      <label for="title">Title:</label>
+      <input name="title" id="title" type="text" maxlength="150" />
+      <label for="bodytext">Body Text:</label>
+      <textarea name="bodytext" id="bodytext"></textarea>
+      <input type="submit" value="Create This Entry!" />
+    </form>
+
+    ADMIN_FORM;
+}
+  public function write($p) {
+    if ( $p['title'] )
+      $title = mysql_real_escape_string($p['title']);
+    if ( $p['bodytext'])
+      $bodytext = mysql_real_escape_string($p['bodytext']);
+    if ( $title && $bodytext ) {
+      $created = time();
+      $sql = "INSERT INTO testDB VALUES('$title','$bodytext','$created')";
+      return mysql_query($sql);
+    } else {
+      return false;
+    }
+}
+  
+  public function display_public() {
+    $q = "SELECT * FROM testDB ORDER BY created DESC LIMIT 3";
+    $r = mysql_query($q);
+
+
+    if ( $r !== false && mysql_num_rows($r) > 0 ) {
+      while ( $a = mysql_fetch_assoc($r) ) {
+        $title = stripslashes($a['title']);
+        $bodytext = stripslashes($a['bodytext']);
+
+        $entry_display .= <<<ENTRY_DISPLAY
+
+
+      <h2>$title</h2>
+      <p>
+      $bodytext
+      </p>
+
+  ENTRY_DISPLAY;
+       }
+      } else {
+        $entry_display = <<<ENTRY_DISPLAY
+
+     <h2>This Page Is Under Construction</h2>
+      <p>
+        No entries have been made on this page. 
+        Please check back soon, or click the
+        link below to add an entry!
+      </p>
+
+    ENTRY_DISPLAY;
+      }
